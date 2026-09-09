6a0524a02ecd77c2d08d43902e9d6c481c3e9de6
ashokdas90t-prog <ashokdas90t@gmail.com>
2026-09-10 02:14:26 +0600

    Create MainActivity.java

---
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.flashlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText(
+                "Double Tap Flashlight\n\n" +
+                "ফোনে দুইবার ট্যাপ করলে Flashlight চালু/বন্ধ হবে।"
+        );
+        textView.setTextSize(22);
+        textView.setPadding(40, 100, 40, 40);
+
+        setContentView(textView);
+    }
+}
